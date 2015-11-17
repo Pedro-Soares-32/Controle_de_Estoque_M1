9748724fc53c7f514f34a2724cf0a825593712d4
pedro.soares <pedro.soares@SNSOO1EP01VM130.saojose.sc.senai.br>
2015-11-17 10:32:18 -0200

    Criação da conexão com o banco de dados

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
+<?php
+    include './includes/Global.php';
+    $title = "Controle de Estoque";
+    include './template/header.php';
+?>
 
-        
+<h1>Hello World!</h1>
+
+<?php
+    include './template/footer.php';
+?>
